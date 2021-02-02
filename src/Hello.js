4b02bad5f01e3c3cdf10dcb58a4f75e9231bfb1b
MMcClure11 <mcclme02@luther.edu>
2021-02-02 11:08:31 -0600

    Add Hello component

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+//Build a react class component called Hello
+//that returns an h1 with "Hello"
+
+import React, { Component } from 'react'
+
+export default class Hello extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
