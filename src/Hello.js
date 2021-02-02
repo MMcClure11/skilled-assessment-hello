a2b3990397b29ae60e42bc1ca3957013cec2d719
MMcClure11 <mcclme02@luther.edu>
2021-02-02 11:18:58 -0600

    Add constructor with state

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,22 @@
 //Build a react class component called Hello
 //that returns an h1 with "Hello"
 
+// make a constructor, assume it accepts props, with state helloTranslations set to an empty array
+
+// assume you have an endpoint of this.props.helloTranslations = 'https://hello/translations'
+// What kind of function would you set this up in?
+// componentDidMount()
+// 
 import React, { Component } from 'react'
 
 export default class Hello extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      helloTranslations: []
+    }
+  }
 
   render() {
     return(
